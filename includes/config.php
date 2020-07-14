6e471d1704cfec982fb9b1e2cb44c372d691153c
山田隆史 <takashiyamada@yamadatakashinoMacBook-Air.local>
2020-07-14 15:35:34 +0900

    update config

--- includes/config.php
+++ includes/config.php
@@ -4,14 +4,14 @@
 
 	$timezone = date_default_timezone_set("Asia/Tokyo");
 
-	$db = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$host = $db['host'];
-	$user = $db['user'];
-	$password = $db['pass'];
-	$dbname = $db['dbname'];
-
+	// $db = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	// $host = $db['host'];us-cdbr-east-02.cleardb.com
+	// $user = $db['user'];b9203a4216854e
+	// $password = $db['pass'];bb9d8c14
+	// $dbname = $db['dbname'];heroku_43a8a15253d2abe
+	// mysql://b9203a4216854e:bb9d8c14@us-cdbr-east-02.cleardb.com/heroku_43a8a15253d2abe?reconnect=true
 	
-	$con = mysqli_connect($host, $user, $password, $dbname);
+	$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b9203a4216854e", "bb9d8c14", "heroku_43a8a15253d2abe");
 	if (mysqli_connect_errno()) {
 		echo "Failed to connect:".mysqli_connect_errno();
 	}
