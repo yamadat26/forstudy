85905043a9d51d8a63dbcf725b0746566ccadf21
山田隆史 <takashiyamada@yamadatakashinoMacBook-Air.local>
2020-07-14 13:53:10 +0900

    mysqli change

--- includes/config.php
+++ includes/config.php
@@ -6,18 +6,18 @@
 
 	$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
   $db['dbname'] = ltrim($db['path'], '/');
-  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-  $user = $db['user'];
-  $password = $db['pass'];
-  $options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-	);
+  // $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+  // $user = $db['user'];
+  // $password = $db['pass'];
+  // $options = array(
+  //   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  //   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  //   PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+	// );
 	
-	// $con = mysqli_connect("localhost", "root", "root", "slotify");
-	$con = new PDO($dsn,$user,$password,$options);
-	// if (mysqli_connect_errno()) {
-		// echo "Failed to connect:".mysqli_connect_errno();
-	// }
+	$con = mysqli_connect("$db['host']", "$db['user']", "$db['pass']", "dbname={$db['dbname']");
+	//$con = new PDO($dsn,$user,$password,$options);
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect:".mysqli_connect_errno();
+	}
 ?>
